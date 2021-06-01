12b4dfa96106fd198be9551cd16ae5a1c0b3aeec
Tomas Perez <tomasitopg6@gmail.com>
2021-06-01 09:57:15 +0000

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById('create-task-form')
+  let submit = document.querySelector("input[type=submit]")
+
+  form.addEventListener('submit', function(event) {
+    addNewTaskToList();
+    event.preventDefault();
+    clearTaskInput();
+  });
+});
+
+function addNewTaskToList() {
+ let task = document.getElementById('new-task-description').value
+ let taskList = document.getElementById('tasks')
+ let newLi = document.createElement('li')
+
+ let newTask = taskList.appendChild(newLi)
+ newTask.innerHTML = task;
+}
+
+function clearTaskInput() {
+ document.getElementById('new-task-description').value = ""
 });
